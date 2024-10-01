09c47f91bb636fb1e387dbc2d2c4a605091d0b4f
Sparkl3Off <danielrebel1605@gmail.com>
2024-10-01 00:20:23 -0300

    ejemplo hola mundo

---
+++ index.js
@@ -0,0 +1,12 @@
+const fn = () => {
+  const miString = "Soy un string!"
+  let b = miString + " muy feliz"
+  console.log(b)
+}
+
+fn()
+
+const clickeando = () => {
+  let a = "chanchito feliz"
+  console.log(a)
+}
